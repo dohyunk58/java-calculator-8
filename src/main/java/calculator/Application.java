ee6605b53555a41d19d2f1b63222f6a9de894211
dohyunk58 <hedge3x@gmail.com>
2025-10-20 04:01:28 +0900

    fix: BufferedWriter 제거
    과제 제출 중 문제 해결을 위해 BufferedWriter 대신 System.out.println을 사용함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,11 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        // 출력을 위한 BufferedWriter 객체 선언
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("덧셈할 문자열을 입력해 주세요.\n");
-        bw.flush();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         // 입력값을 저장할 변수 str
         String str = Console.readLine();
@@ -18,8 +13,6 @@ public class Application {
         StringCalculator calculator = new StringCalculator();
         long result = calculator.add(str);
 
-        bw.write("결과 : " + result);
-        bw.flush();
-        bw.close();
+        System.out.println("결과 : " + result);
     }
 }
